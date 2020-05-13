16d199dcfdd78fab1dfd5497b8ebec520b25fc92
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-13 17:43:17 +0200

    Known bug#1
    bug#1: repeated persons in a message in simulation algorithm.

--- src/simulator.py
+++ src/simulator.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 from collections import defaultdict
 import random
 
-# corroutine(msg)
+
 def simulation_first(persons: int, ring_order: int, message_list: List[int]) -> List[List[int]]:
     result = []
     choices = {i for i in range(persons)}
@@ -13,6 +13,7 @@ def simulation_first(persons: int, ring_order: int, message_list: List[int]) -> 
         result.append(actual)
     return result
 
+
 def simulation(persons: int, ring_order: int, message_list: List[int]) -> List[List[int]]:
     result = []
     choices = [i for i in range(persons)]
@@ -21,13 +22,26 @@ def simulation(persons: int, ring_order: int, message_list: List[int]) -> List[L
         if msg not in actual:
             actual[0] = msg
         result.append(actual)
-        choices.append(actual)
+        choices.extend(actual)
     return result
 
-def simulation_to_dictionary(simulation: List[List[int]]) -> Dict[int, List[int]]:
-    dic = defaultdict(list)
+
+def simulation_to_dictionary(persons: int, simulation: List[List[int]]) -> Dict[int, List[int]]:
+    res = [[]] * persons
     for i, ls in enumerate(simulation):
         for person in ls:
-            dic[person].append(i)
-    return dic
+            res[person].append(i)
+    return res
+
+
+def probability_of_all(persons: int, message_list: List[int], simulation: List[List[int]]):
+    sim = simulation_to_dictionary(persons, simulation)
+    msgcount = [0] * persons
+    for i in message_list:
+        msgcount[i] += 1
+    res = []
+    for i in range(persons):
+        res.append(msgcount[i] / len(sim[i]))
+    return res
+
 
