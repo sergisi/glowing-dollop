b4a530bf99d35adbc78b8682ac25ab25f4b5718f
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-19 17:42:55 +0200

    Done tests:Done tests:

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -30,7 +30,25 @@ class MyTestCase(unittest.TestCase):
         weights = or_weights.copy()
         for i in range(len(weights)):
             wlen = i + 1
-            print("=====", wlen)
-            print(weights)
-            self.assertEqual(len(sim.get_choices(weights, wlen)), wlen, f'{len(sim.get_choices(weights, wlen))} {wlen}')
+            self.assertEqual(len(sim.get_choices(weights, wlen)), wlen)
             weights = or_weights.copy()
+
+    def test_get_message_count(self):
+        persons = 8
+        messages = [0] + 3 * [2] + [1] * 4 + 2 * [7] + [4] * 5
+        self.assertEqual([1, 4, 3, 0, 5, 0, 0, 2], sim.get_messagecount(persons, messages))
+
+    def test_simulation_to_dictionary(self):
+        persons = 8
+        simulation = [[1, 2, 3, 4], [0, 1, 3, 6], [7, 3, 4, 6]]
+        self.assertEqual([1, 2, 1, 3, 2, 0, 2, 1], sim.simulation_to_dictionary(persons, simulation))
+
+    def test_probability_of_all(self):
+        persons = 8
+        messages = [2, 3, 7]
+        simulation = [[1, 2, 3, 4], [0, 1, 3, 6], [7, 3, 4, 6]]
+        prob = sim.probability_of_all(persons, messages, simulation)
+        for p in messages:
+            self.assertNotEqual(0, prob[p])
+        for nm in set(range(8)) - set(messages):
+            self.assertEqual(0, prob[nm])
