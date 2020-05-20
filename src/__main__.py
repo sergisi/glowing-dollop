3db747cd3fdfdd306139f220a760c48cd8bec062
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-20 17:22:14 +0200

    more stats

--- src/__main__.py
+++ src/__main__.py
@@ -25,13 +25,18 @@ def main(num_people, k, max_msg, num_loop, s):
     probs = list(map(lambda x: x / num_loop,
                      reduce(lambda ls1, ls2: [a + b for a, b in zip(ls1, ls2)],
                             probability(num_people, k, messages, num_loop))))
-    print("PROBABILITIES")
-    print(f"p1: {probs[p1]}, p5: {probs[p5]}, p15: {probs[p15]}")
+    print(f"s: {s},\tk: {k},\tp1: {probs[p1]},\tp5: {probs[p5]},\tp15: {probs[p15]}")
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        for s in map(lambda x: x / 10, range(13, 21)):
+            for k in [4, 6, 8, 10, 12]:
+                main(200, k, 15, 10, s)
+        sys.exit()
     if len(sys.argv) != 6:
         print("Usage: python -m src <num_people> <k> <max_msg> <num-loop> <s-zipf> ")
+        print("Usage: python -m src")
         sys.exit()
     num_people, k, max_msg, num_loop = list(map(int, sys.argv[1:-1]))
     s = float(sys.argv[-1])
