24b520961f46e102c854624a916ffcf7525d1c98
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-20 16:29:11 +0200

    __main__

--- src/__main__.py
+++ src/__main__.py
@@ -2,18 +2,24 @@ import random
 import sys
 import src.simulator as sim
 import src.zipf as zf
+from functools import reduce
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python -m src <num_people> <k> <max_msg> <num-loop>")
-    num_people, k, max_msg, num_loop = list(map(int, sys.argv[1:]))
-    messages = zf.zipf(num_people, max_msg)
+def probability(num_people, k, messages, num_loop):
     for i in range(num_loop):
         rnd_msgs = random.sample(messages, k=len(messages))
         sims = sim.simulation(num_people, k, rnd_msgs)
-        print(sim.probability_of_all(num_people, rnd_msgs, sims))
+        yield sim.probability_of_all(num_people, rnd_msgs, sims)
 
 
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python -m src <num_people> <k> <max_msg> <num-loop>")
+    num_people, k, max_msg, num_loop = list(map(int, sys.argv[1:]))
+    messages = zf.zipf(num_people, max_msg, 1.5)
+    probs = list(map(lambda x: x / num_loop,
+        reduce(lambda ls1, ls2: [a + b for a, b in zip(ls1, ls2)], probability(num_people, k, messages, num_loop))))
+    print(probs)
 
-
+if __name__ == '__main__':
+    main()
